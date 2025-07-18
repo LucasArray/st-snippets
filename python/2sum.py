3641700afa1e6030d04aed8d9f2f336a426b5002
Lucas Dearaujo <lucasaraujo45@outlook.com>
2025-07-18 12:24:35 -0400

    test file

--- python/2sum.py
+++ python/2sum.py
@@ -5,19 +5,3 @@ def two_sum(nums: list[int], target: int) -> list[int]:
         if complement in num_to_index:
             return [num_to_index[complement], i]
         num_to_index[num] = i
-
-def two_sum(nums: list[int], target: int) -> list[int]:
-    num_to_index = {}
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in num_to_index:
-            return [num_to_index[complement], i]
-        num_to_index[num] = i
-
-def two_sum(nums: list[int], target: int) -> list[int]:
-    num_to_index = {}
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in num_to_index:
-            return [num_to_index[complement], i]
-        num_to_index[num] = i
---
+++ python/2sum_test.py
@@ -0,0 +1,23 @@
+def two_sum(nums: list[int], target: int) -> list[int]:
+    num_to_index = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_to_index:
+            return [num_to_index[complement], i]
+        num_to_index[num] = i
+        
+def two_sum(nums: list[int], target: int) -> list[int]:
+    num_to_index = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_to_index:
+            return [num_to_index[complement], i]
+        num_to_index[num] = i
+        
+def two_sum(nums: list[int], target: int) -> list[int]:
+    num_to_index = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_to_index:
+            return [num_to_index[complement], i]
+        num_to_index[num] = i
